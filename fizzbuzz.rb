526ac517d5caabc0c84719e999a8944015e44485
Zach Bayard <zbayard@gmail.com>
2020-10-01 18:05:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
+  if (int % 3 == 0) && (int % 5 == 0)
+    "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
-  else int % 5 == 0 && int % 3 == 0
-    "FizzBuzz"
+  else int % 3 == 0
+    "Fizz"
   end
 end
